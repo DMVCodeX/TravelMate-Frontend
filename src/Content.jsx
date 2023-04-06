c8f7ec9266eba2a96d17ad1e01c3e3f9fd92a4bb
DIANELLY VENTURA <DMVCode@outlook.com>
2023-04-06 00:00:53 -0400

    add header, footer and content folder

---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export function Content() {
+  return (
+    <div>
+      <h1>Welcome to TravelMate!</h1>
+    </div>
+  );
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div>
+      <footer className="center-text">
+        <p>Copyright 2023</p>
+      </footer>
+    </div>
+  );
+}
