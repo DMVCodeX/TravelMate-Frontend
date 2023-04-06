eb0849674f676134f97d0e7865c6ce7d2bbfcfc3
DIANELLY VENTURA <DMVCode@outlook.com>
2023-04-06 01:11:37 -0400

    move sign up /signup route

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Content } from "./Content";
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Content />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Content />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,13 @@
 import { SignUp } from "./SignUp";
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
   return (
-    <div>
-      <SignUp />
+    <div className="container">
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
